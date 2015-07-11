b276a5c5bd03db7b69ef57bb01424188832c1f0f
Annie Lydens <aelydens@gmail.com>
2015-07-11 15:54:45 -0500

    Static routes and shareJS setup

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var express = require('express.io');
 var http = require('http');
 var path = require('path');
 var bookshelf = require('./bookshelf');
-var Question = require('./models/question.js')
+var Exercise = require('./models/exercise.js')
+var localStorage = require('localStorage');
 
 var app = express();
 app.http().io()
@@ -28,21 +29,6 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.io.route('ready', function (req) {
-  req.io.join(req.data.room);
-  req.io.room(req.data.room).broadcast('announce', {
-    message: req.data.username + ' just joined the room. ',
-    username: req.data.username
-  });
-});
-
-app.io.route('sendMessage', function (req) {
-  req.io.join(req.data.room);
-  req.io.room(req.data.room).broadcast('newMessage', {
-    message: req.data.message,
-    username: req.data.username
-  })
-});
 
 app.get('/', function (req, res) {
   res.render('welcome', { title: 'Code Czar' });
@@ -53,21 +39,54 @@ app.get('/index', function (req, res) {
 });
 
 app.get('/questions', function(req, res, next) {
-  Question.collection().fetch().then(function(questions) {
-    res.render('questions/index', {questions: questions.toJSON()});
+  Exercise.collection().fetch().then(function(exercises) {
+    res.render('questions/index', {exercises: exercises.toJSON()});
+  }).catch(function(error) {
+    console.log(error);
+    res.send('An error occurred');
+  });
+});
+
+app.get('/questions/new', function(req, res, next) {
+  res.render('questions/new', { title: 'Code Czar' });
+});
+
+app.post('/questions', function(req, res, next) {
+  Exercise.forge({
+    title: req.body['exercise']['title'],
+    description: req.body['exercise']['description'],
+    link: req.body['exercise']['link'],
+    tag_id: req.body['exercise']['tag_id'],
+    upvotes: '0'
   })
+  .save()
+  .then(function(exercise) {
+    res.redirect('/questions');
+  })
+  .catch(function(err) {
+    return console.error(err);
+  });
 });
 
 app.get('/questions/:id', function (req, res) {
-  res.render('question', { title : req.params.id, username : req.query.username });
+  new Exercise({id: req.params.id})
+  .fetch()
+  .then(function(exercise) {
+    res.render('questions/show', {exercise: exercise.toJSON()});
+  });
 });
 
-app.get('/rooms/:id', function (req, res) {
-  res.render('room', { title : req.params.id, username : req.query.username });
-});
 
-app.get('/battle', function (req, res) {
-  res.render('battle', { title : 'Code Czar'});
-});
+var connect = require('connect'),
+    sharejs = require('share').server;
+
+var options = {db: {type: 'none'}}
+
+var server = app.use(
+  connect.logger(),
+  connect.static(__dirname + '/public')
+);
+
+sharejs.attach(server, options);
 
 app.listen(app.get('port'));
